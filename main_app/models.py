7244833bf72854269a3a4a3db7a7fcdee1ea5644
AC244 <ac03basketball24@gmail.com>
2024-12-10 13:20:06 -0600

    home rendering

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Shoe Model
+class Shoe(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=50, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    image_url = models.URLField(blank=True, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+# Favorite Shoe Model
+class FavoriteShoe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    shoe = models.ForeignKey(Shoe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.shoe.name}"
+
